a32637ef219ddc07a282afae7f0d19aab95631db
Muthu-AMMEW <muthu.ammew@gmail.com>
2025-07-11 15:45:39 +0530

    Tawk update

--- public/js/script.js
+++ public/js/script.js
@@ -175,7 +175,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
   var s1 = document.createElement("script"),
     s0 = document.getElementsByTagName("script")[0];
   s1.async = true;
-  s1.src = "https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6";
+  s1.src = "https://embed.tawk.to/683f03323ab1e8190d1ba353/1isr28vsh";
   s1.charset = "UTF-8";
   s1.setAttribute("crossorigin", "*");
   s0.parentNode.insertBefore(s1, s0);
